8cf198d12c4a702cca17e02e83a8548303d8eac3
salihatgl <salihatigli55@gmail.com>
2023-10-17 17:59:07 +0300

    Manual visits

--- resources/js/Pages/Education.vue
+++ resources/js/Pages/Education.vue
@@ -1,19 +1,94 @@
 <template>
-  <header style="border: 1px solid black; text-align: center;">
-      <Link style="margin-right: 20px; text-decoration: none; color: black;" href="\Home">Anasayfa</Link>
-    <Link style="margin-right: 20px; text-decoration: none; color: black;" href="\Education">Okullar</Link>
-    <Link style="margin-right: 20px; text-decoration: none; color: black;" href="\Programming">Programlama Dilleri</Link>
+  <header style="list-style-type: none; margin: 0; padding: 20px; overflow: hidden; background-color: #333; position: fixed; top: 0px; width: 100%;">
+    <Link style="margin-right: 20px; text-decoration: none; color: white;" href="\Home">Anasayfa</Link>
+    <Link style="margin-right: 20px; text-decoration: none; color: white;" href="\Education">Okullar</Link>
+    <Link style="margin-right: 20px; text-decoration: none; color: white;" href="\Programming">Programlama Dilleri</Link>
   </header>
   <h1>Okullar</h1>
   <p>Meram Atatürk MYO</p>
-  <p  style="margin-bottom: 400px;">Konya Teknik Üniversitesi</p>
-  <h1>Okullar</h1>
-  <p>Meram Atatürk MYO</p>
-  <p  style="margin-bottom: 400px;">Konya Teknik Üniversitesi</p>
-  
-  
+  <p>Konya Teknik Üniversitesi</p>
 
-</template>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo et similique aliquid delectus officiis,
+    eos ullam neque. Reprehenderit error rerum assumenda asperiores doloribus nemo illo vel nobis quaerat
+    consequuntur quis, repudiandae tempore! Laudantium praesentium provident rem natus tempora fugiat molestias
+    autem sit. Omnis consequuntur quis minima quo itaque dolor optio quia dolorum similique quibusdam praesentium
+    incidunt sequi nisi nostrum illo quas, repudiandae in sunt corporis commodi eveniet quasi voluptas odio dolores.
+    Nesciunt doloribus vero commodi aperiam? Necessitatibus odio, accusamus nihil vel, maxime explicabo a voluptate
+    facilis libero omnis ad. Ea porro, consequatur similique architecto hic distinctio aut. Rerum numquam, modi natus
+    non quas delectus deserunt tempora voluptate iusto ea neque laborum voluptates deleniti cum suscipit. Quas,
+    voluptates.
+    Debitis dolor iusto soluta sapiente error earum, veniam dolores quo molestias nemo dolorum labore aliquid commodi
+    providen
+    t nobis reiciendis voluptatum id voluptates mollitia natus aliquam? Ducimus minima quis animi nam, aut vel labore
+    magnam,
+    officia eligendi nulla quaerat dolorem, ipsum nemo iusto molestias natus consectetur obcaecati non. Reiciendis ipsa
+    fugit
+    optio deserunt explicabo velit placeat odio beatae delectus numquam laborum quisquam, dignissimos eius, fuga veritatis
+    ex
+    et eligendi ipsum. Harum aut beatae similique illo in adipisci commodi voluptate quis molestias totam facere vel autem
+    odit explicabo quisquam maxime rem animi consequatur, sunt ipsum quo, veritatis architecto. Quis veniam assumenda
+    quam,
+    accusamus facere laudantium sint at modi eaque rerum, ea odio quisquam libero necessitatibus officia. Blanditiis
+    corrupti
+    laudantium sunt laborum a excepturi maiores. Sequi necessitatibus voluptas, cumque laborum repudiandae explicabo
+    aperiam
+    dolore deleniti aliquid commodi a doloremque voluptatibus blanditiis maxime nam harum porro nisi ad est quia. Quasi
+    deleniti
+    quidem, ab nobis in cupiditate sed nihil aliquam ea aperiam voluptatum totam nesciunt iste amet id nam, sunt
+    laboriosam
+    consectetur veniam numquam vero nostrum quod? Ullam aliquid eveniet sunt illo quibusdam totam illum mollitia labore!
+    Animi
+    ducimus, possimus nam nostrum incidunt nihil corporis quia suscipit perspiciatis quam consequatur minus neque
+    cupiditate quas
+    error, voluptates esse sapiente iste eum saepe ab ea maiores libero? Doloremque veniam a quae fugiat esse ducimus
+  aperiam,
+  facilis libero magnam architecto ex. Deleniti accusantium, quos dolor nulla unde laudantium minus. In quo maxime
+  tempore,
+  iusto nulla doloremque eveniet veritatis voluptates velit eligendi pariatur blanditiis cum beatae aspernatur culpa
+  sunt nobis
+  reiciendis, quidem, facilis sed at. Laborum, sequi repellat eius similique distinctio, quidem dolorum accusamus
+  voluptas blanditiis, in libero cupiditate? Obcaecati quos enim voluptatum assumenda cupiditate id ex tempore nostrum,
+  debitis est quidem odit culpa? Ea ducimus quas cumque ut ullam totam, reprehenderit cupiditate mollitia sed illum.
+  Provident cum magnam nostrum, corrupti soluta minima quis enim distinctio amet suscipit maiores optio dolores expedita
+  eaque porro! Aspernatur unde perspiciatis saepe quae recusandae eveniet, enim voluptatum minus eum id. Rem, cum.
+  Praesentium possimus excepturi iusto est id deleniti sapiente fugit dolore aperiam odit quaerat quam iure, ab hic
+  itaque dolorem maiores dignissimos ratione? Magni tenetur facere quo, nostrum, ex molestiae aliquid ratione culpa
+  repellendus et ducimus facilis. Ab, consectetur qui similique tempore, officia vel sint quibusdam velit id eum,
+  officiis sequi laboriosam? Nihil quae nemo unde beatae. Placeat dolorum repellendus provident reiciendis tempora
+  consequuntur aspernatur nulla labore culpa repellat! Quaerat distinctio sunt nihil doloremque cumque tenetur tempora,
+  voluptatibus ea quidem tempore. Libero expedita explicabo facilis eum, iste incidunt laborum exercitationem dolorem?
+  Labore voluptates deserunt laudantium eum quos eveniet odit, repudiandae accusantium, provident atque autem ullam
+  corrupti sapiente. Odit sed eum id ducimus fuga facere tenetur suscipit cum nesciunt nulla, maxime ipsam voluptate
+  praesentium ab nam blanditiis ipsum temporibus cupiditate rerum enim qui natus distinctio fugit facilis! Blanditiis,
+  perspiciatis magnam alias necessitatibus ullam excepturi eius. Deleniti nostrum itaque ea eius dolorem eum cum quo,
+  rem molestias deserunt. Quasi expedita iure nemo sed. Sequi totam nostrum voluptatem similique pariatur alias optio
+  reprehenderit iusto aperiam, ipsa voluptatibus ullam? Perspiciatis eligendi, ex beatae, architecto magni id dolorem
+  minus ullam excepturi cumque ipsam voluptate. Similique repellendus quae numquam modi iure, ipsa quidem alias
+  distinctio provident tempore illo voluptatem voluptatibus aliquam quo eum nobis, odio nihil eligendi, ducimus nisi
+  facilis quis dolorum eius! Deserunt adipisci quibusdam, ea exercitationem nesciunt quod ratione facilis ducimus error.
+  Nostrum hic ullam, vel et totam pariatur eos fuga ipsa reiciendis illo placeat aliquid eum ab facere eius doloremque,
+  corrupti ex quae quod sint alias recusandae blanditiis earum sapiente! Et facilis dignissimos, dolore asperiores ea
+  voluptatibus voluptatum quisquam iste sequi exercitationem vel maiores, delectus id maxime culpa officiis doloribus
+  dolorem consequatur voluptate repudiandae! Officia accusamus neque perferendis harum consequuntur provident sed
+  perspiciatis velit quasi. Facilis dolor iste accusamus impedit incidunt recusandae animi quam ipsum aut rem reiciendis
+  cumque, officiis deserunt earum neque, non aperiam. Iure, fuga excepturi quam fugit dolor illo dolore itaque natus
+  quae assumenda officiis enim illum tempora numquam velit adipisci, a quaerat in nam voluptatum eum? Labore obcaecati
+  eveniet asperiores maxime hic necessitatibus quibusdam minus eligendi. Fugit modi laborum ducimus repudiandae numquam
+  cupiditate harum debitis, itaque cum eius aperiam exercitationem error assumenda cumque, iusto ea voluptas mollitia
+  ipsam maxime nostrum impedit aspernatur. Obcaecati iure autem accusamus impedit reiciendis dolores laudantium deleniti
+  eius adipisci aperiam aliquid delectus ab numquam quia amet maiores rerum ullam quae id debitis tempora, provident
+  quas animi? Mollitia culpa facere tempora alias laborum provident? Non beatae temporibus commodi reiciendis maxime,
+  repellat, tenetur sapiente repudiandae explicabo quam quibusdam blanditiis enim ipsum molestiae animi voluptate, in
+  eaque sed cum esse. Dolore reiciendis repellat impedit, beatae repellendus sapiente autem libero voluptate itaque
+  deserunt rerum, eveniet velit optio consequuntur amet quidem totam. Molestias aliquam hic porro necessitatibus ad
+  voluptates voluptas saepe? Numquam temporibus, maxime quidem eum dolorum corrupti hic reprehenderit molestiae sit vel
+  tempora earum explicabo amet voluptatibus inventore ratione adipisci molestias omnis nulla neque porro ab libero
+  necessitatibus voluptate? Pariatur culpa mollitia, error consequatur expedita sapiente quas ducimus eligendi possimus
+  eum veritatis recusandae odio sit molestiae! Natus hic necessitatibus fuga, corporis sunt ducimus illo, voluptate
+  possimus explicabo, adipisci totam ipsa perferendis sapiente eos animi veritatis vitae dolor aut voluptates deleniti
+  impedit distinctio dolorem nostrum? Velit nulla vero quis architecto minus natus consequatur, ex commodi at fugit
+  consectetur. Animi blanditiis nostrum odio sequi voluptatum!</p>
+<p style="margin-bottom: 400px;">Konya Teknik Üniversitesi</p></template>
 <script setup>
 import { Link } from '@inertiajs/vue3'
 </script>
--- resources/js/Pages/Programming.vue
+++ resources/js/Pages/Programming.vue
@@ -1,10 +1,10 @@
 <template>
-    <header style="border: 1px solid black; text-align: center;">
-      <Link style="margin-right: 20px; text-decoration: none; color: black;" href="\Home">Anasayfa</Link>
-      <Link style="margin-right: 20px; text-decoration: none; color: black;" href="\Education">Okullar</Link>
-      <Link style="margin-right: 20px; text-decoration: none; color: black;" href="\Programming">Programlama Dilleri</Link>
+    <header style="list-style-type: none; margin: 0; padding: 20px; overflow: hidden; background-color: #333; position: fixed; top: 0px; width: 100%;">
+      <Link style="margin-right: 20px; text-decoration: none; color: white;" href="\Home">Anasayfa</Link>
+      <Link style="margin-right: 20px; text-decoration: none; color: white;" href="\Education">Okullar</Link> 
+      <Link style="margin-right: 20px; text-decoration: none; color: white;" href="\Programming">Programlama Dilleri</Link>
     </header>
-    <h1>Programlama Dilleri</h1>
+    <h1 style="margin-top: 100px;">Programlama Dilleri</h1>
     <p>HTML, CSS, C#, Laravel</p>
 </template>
 <script setup>
